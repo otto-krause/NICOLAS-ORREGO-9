af1df0aadca3ba72b0a5954949d855632d6469d1
nicolasorregoa <87020804+nicolasorregoa@users.noreply.github.com>
2021-09-01 11:58:44 -0300

    Update ej5while.c

--- ej5while.c
+++ ej5while.c
@@ -1,21 +1,2 @@
-  
-#include<stdio.h>
 
-int main (){
-	
-	int base, potencia, i, resultado=1;
-	
-	printf("ingrese dos numeros: ");
-	scanf("%d %d", &base, &potencia);
-	
-	for(i=0;i<potencia;i++){
-		
-		resultado= resultado * base;
-	}
-		
-		printf("resultado: %d", resultado);	
-	
-	
-	
-	return 0;
-}
+
