c8a25da236bbecd04f59e16f5890967465bbb53d
nicolasorregoa <87020804+nicolasorregoa@users.noreply.github.com>
2021-09-01 21:44:40 -0300

    Update ej5while.c

--- ej5while.c
+++ ej5while.c
@@ -1,2 +1,20 @@
+#include<stdio.h>
+int main(){
+ int total=1,n1,n2,i=0;
+
+ printf("ingrese la base: ");
+ scanf("%d",&n1);
+ printf("ingrese el exponente: ");
+ scanf("%d",&n2);
+
+ while(i<n2){
+   total=total*n1;
+
+   i++;
+ }
+ printf("el valor es: %d",total);
+
+  return 0;
+}
 
 
